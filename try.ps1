9ac7cb72b907f434a8fd827dde5e75c02e53e74d
mosquito-michaelangelo <michaelmosquito147@gmail.com>
2025-01-12 20:43:02 +0800

    fix search bar position

--- try.ps1
+++ try.ps1
@@ -1,4 +1,4 @@
-﻿# Import required assemblies
+# Import required assemblies
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
@@ -24,18 +24,50 @@ $listBox.Size = New-Object System.Drawing.Size(600, 500)
 $listBox.Location = New-Object System.Drawing.Point(270, 50)
 $form.Controls.Add($listBox)
 
-# Create a TextBox for the search functionality
+# Create a TextBox for the search functionality (right side)
 $searchBox = New-Object System.Windows.Forms.TextBox
 $searchBox.Size = New-Object System.Drawing.Size(200, 20)
-$searchBox.Location = New-Object System.Drawing.Point(10, 10)
+$searchBox.Location = New-Object System.Drawing.Point(604, 8)  # Place search box on the left of toolbar
 $form.Controls.Add($searchBox)
 
-# Create a Button for initiating the search
+# Create a Button for initiating the search (right side)
 $searchButton = New-Object System.Windows.Forms.Button
 $searchButton.Text = "Search"
-$searchButton.Size = New-Object System.Drawing.Size(75, 25)
-$searchButton.Location = New-Object System.Drawing.Point(806, 10)
+$searchButton.Size = New-Object System.Drawing.Size(70, 20)
+$searchButton.Location = New-Object System.Drawing.Point(806, 9)  # Place search button next to the search box
 $form.Controls.Add($searchButton)
+
+# Create a toolbar panel (smaller height)
+$toolbar = New-Object System.Windows.Forms.Panel
+$toolbar.Size = New-Object System.Drawing.Size(900, 30)  # Reduced height of the toolbar
+$toolbar.Location = New-Object System.Drawing.Point(0, 0)
+$toolbar.BackColor = [System.Drawing.Color]::DarkGray
+$form.Controls.Add($toolbar)
+
+# Add buttons to the toolbar
+$createFolderButton = New-Object System.Windows.Forms.Button
+$createFolderButton.Text = "New Folder"
+$createFolderButton.Size = New-Object System.Drawing.Size(100, 25)  # Adjusted button size
+$createFolderButton.Location = New-Object System.Drawing.Point(10, 3)
+$toolbar.Controls.Add($createFolderButton)
+
+$copyButton = New-Object System.Windows.Forms.Button
+$copyButton.Text = "Copy"
+$copyButton.Size = New-Object System.Drawing.Size(100, 25)  # Adjusted button size
+$copyButton.Location = New-Object System.Drawing.Point(120, 3)
+$toolbar.Controls.Add($copyButton)
+
+$pasteButton = New-Object System.Windows.Forms.Button
+$pasteButton.Text = "Paste"
+$pasteButton.Size = New-Object System.Drawing.Size(100, 25)  # Adjusted button size
+$pasteButton.Location = New-Object System.Drawing.Point(230, 3)
+$toolbar.Controls.Add($pasteButton)
+
+$deleteButton = New-Object System.Windows.Forms.Button
+$deleteButton.Text = "Delete"
+$deleteButton.Size = New-Object System.Drawing.Size(100, 25)  # Adjusted button size
+$deleteButton.Location = New-Object System.Drawing.Point(340, 3)
+$toolbar.Controls.Add($deleteButton)
 
 # Function to search for files and folders
 function Search-FilesAndFolders {
